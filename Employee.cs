45185e7bec16a49109c8de17cae4a79671426db5
Shubhangi1256 <108929358+Shubhangi1256@users.noreply.github.com>
2022-07-08 15:46:11 +0530

    This is employee class

---
+++ Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL
+{
+    public class Employee
+    {
+       /* public int EMPNO { get; set; }
+        public string ENAME { get; set; }
+        public string JOB { get; set; }
+        public int MGR { get; set; }
+        public DateTime HIREDATE { get; set; }
+        public double SAL { get; set; }
+        public double COMM { get; set; }
+        public int DEPTNO { get; set; }*/
+
+
+        public int sid { get; set; }
+        public string sname { get; set; }
+        public string addr { get; set; }
+    }
+}
+       
